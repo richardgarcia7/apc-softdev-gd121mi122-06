568c336795e9572c0f9f54ee16ad773cf96d346d
Richard Joseph Garcia <richardpecsongarcia@gmail.com>
2015-07-14 13:09:35 +0800

    changed db

--- practice/rpgarcia/basic/config/db.php
+++ practice/rpgarcia/basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii3basic',
+    'dsn' => 'mysql:host=localhost;dbname=yiibasic3',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
