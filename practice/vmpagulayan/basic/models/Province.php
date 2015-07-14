2de3ecf7a8da1b62d63ed258221830cd2750fe75
vmpagulayan <kmpagulayan@gmail.com>
2015-07-14 13:17:33 +0800

    genarate

---
+++ practice/vmpagulayan/basic/models/Province.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "province".
+ *
+ * @property integer $id
+ * @property string $province_code
+ * @property string $province_description
+ * @property string $provincecol
+ * @property integer $region_id
+ *
+ * @property City[] $cities
+ */
+class Province extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'province';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['province_code', 'province_description', 'provincecol', 'region_id'], 'required'],
+            [['region_id'], 'integer'],
+            [['province_code', 'province_description', 'provincecol'], 'string', 'max' => 100]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'province_code' => 'Province Code',
+            'province_description' => 'Province Description',
+            'provincecol' => 'Provincecol',
+            'region_id' => 'Region ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCities()
+    {
+        return $this->hasMany(City::className(), ['province_id' => 'id']);
+    }
+}
