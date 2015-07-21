c5c11108a8df5290d938fa8220a8eac8d7320bef
Richard Joseph Garcia <richardpecsongarcia@gmail.com>
2015-07-21 12:22:55 +0800

    renamed folder basic

---
+++ practice/midterm_test/Garcia_Richard_Joseph_midterms/ourspace/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=softdev_Garcia_Richard_Joseph_ourspac',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
